eb6b328110ebad38e6f130ced3259296a24f3968
Robert Colgan <colgan@cs.columbia.edu>
2015-11-02 20:05:13 -0500

    Store kernel output for reuse

--- makek_i.m
+++ makek_i.m
@@ -1,8 +1,16 @@
 function k_i = makek_i(i, dataset)
 
+persistent K;
+if isempty(K) K = nan(size(dataset, 1), size(dataset, 1)); end
+
 x_i = dataset(i, :);
-k_i = nan(size(x_i, 1), 1);
-for j = 1:size(dataset, 1)
-    x_j = dataset(j, :);
-    k_i(j) = rbfKernel(x_i, x_j, dataset);
+if isnan(K(:, i))
+    k_i = nan(size(dataset, 1), 1);
+    for j = 1:size(dataset, 1)
+        x_j = dataset(j, :);
+        k_i(j) = rbfKernel(x_i, x_j, dataset);
+    end
+    K(:, i) = k_i;
+else
+    k_i = K(:, i);
 end
