99cf64a739d0e872da0936496f84eb7c73f80fd8
Robert Colgan <colgan@cs.columbia.edu>
2015-11-02 21:02:19 -0500

    Function to calculate total risk

---
+++ calculateRisk.m
@@ -0,0 +1,12 @@
+function riskJ = calculateRisk(X, Y, w, lambda)
+
+N = size(X, 1);
+
+sm = 0;
+for i = 1:N
+    y_i = Y(i);
+    k_i = makek_i(i, X);
+    sm = sm + log(sigmoid(y_i * (w' * k_i)));
+end
+
+riskJ = -1/N * sm + lambda * (w' * w);
