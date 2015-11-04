bb2a5ab528e169c57f3d76326c0ae20079e1dcf7
Robert Colgan <colgan@cs.columbia.edu>
2015-11-03 21:47:45 -0500

    Test accuracy

---
+++ testAccuracy.m
@@ -0,0 +1,23 @@
+function acc = testAccuracy(w, TestX, TestY)
+
+[~] = makek_i(1, TestX, true); % Clear persistent K
+correct = 0;
+for i = 1:size(TestX, 1)
+    x_i = TestX(i, :);
+    y_i = TestY(i, :);
+    
+    % Check Piazza question to make sure ks should be computed from testing
+    % data (if not, remove clearing above)
+    k_i = makek_i(i, TestX);
+    probability = sigmoid(w' * k_i);
+    if probability > .5
+        guess = 1;
+    else
+        guess = -1;
+    end
+    
+    if guess == y_i
+        correct = correct + 1;
+    end
+end
+acc = correct / size(TestX, 1);
