9c943e9032ffa7bd7445e09efdf375602081ca90
Robert Colgan <colgan@cs.columbia.edu>
2015-11-03 22:14:30 -0500

    Only force rbfKernel to recompute kappa once

--- makek_i.m
+++ makek_i.m
@@ -4,6 +4,7 @@ function k_i = makek_i(i, dataset, clearPersistent)
 persistent K
 if isempty(K) || (nargin > 2 && clearPersistent)
     K = nan(size(dataset, 1), size(dataset, 1)); 
+    [~] = rbfKernel(dataset(1, :), dataset(2, :), dataset, true);
 end
 
 x_i = dataset(i, :);
@@ -11,11 +12,7 @@ if isnan(K(:, i))
     k_i = nan(size(dataset, 1), 1);
     for j = 1:size(dataset, 1)
         x_j = dataset(j, :);
-        if nargin > 2
-            k_i(j) = rbfKernel(x_i, x_j, dataset, clearPersistent);
-        else
-            k_i(j) = rbfKernel(x_i, x_j, dataset);
-        end
+        k_i(j) = rbfKernel(x_i, x_j, dataset);
     end
     K(:, i) = k_i;
 else
